c53781b056f174b710c0d394107f94e15e42b716
Kv1st3n <233747346+Kv1st3n@users.noreply.github.com>
2026-06-26 20:21:41 +0200

    update

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+const dragElement = document.getElementById("draggable");
 
+let offsetX = 0;
+let offsetY = 0;
+
+box.addEventListener('pointerdown', (e) => {
+  box.setPointerCapture(e.pointerId);
+  
+  offsetX = e.clientX - box.offsetLeft;
+  offsetY = e.clientY - box.offsetTop;
+  
+  box.addEventListener('pointermove', dragElement);
+  box.addEventListener('pointerup', stopElementDrag);
+});
+
+function dragElement(e) {
+  box.style.left = `${e.clientX - offsetX}px`;
+  box.style.top = `${e.clientY - offsetY}px`;
+}
+
+function stopElementDrag(e) {
+  box.releasePointerCapture(e.pointerId);
+  box.removeEventListener('pointermove', dragElement);
+  box.removeEventListener('pointerup', stopElementDrag);
+}
