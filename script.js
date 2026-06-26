ba214f083a54f97545d3cbf68b7c8bd9f0b04a11
Kv1st3n <233747346+Kv1st3n@users.noreply.github.com>
2026-06-26 20:27:14 +0200

    update

--- script.js
+++ script.js
@@ -4,22 +4,25 @@ let offsetX = 0;
 let offsetY = 0;
 
 box.addEventListener('pointerdown', (e) => {
-  box.setPointerCapture(e.pointerId);
-  
-  offsetX = e.clientX - box.offsetLeft;
-  offsetY = e.clientY - box.offsetTop;
-  
-  box.addEventListener('pointermove', onDrag);
-  box.addEventListener('pointerup', stopElementDrag);
+    box.setPointerCapture(e.pointerId);
+    
+    offsetX = e.clientX - box.offsetLeft;
+    offsetY = e.clientY - box.offsetTop;
+    
+    box.addEventListener('pointermove', onDrag);
+    box.addEventListener('pointerup', stopElementDrag);
 });
 
 function onDrag(e) {
-  box.style.left = `${e.clientX - offsetX}px`;
-  box.style.top = `${e.clientY - offsetY}px`;
+    box.style.position = 'absolute';
+    box.style.margin = '0';
+
+    box.style.left = `${e.clientX - offsetX}px`;
+    box.style.top = `${e.clientY - offsetY}px`;
 }
 
 function stopElementDrag(e) {
-  box.releasePointerCapture(e.pointerId);
-  box.removeEventListener('pointermove', onDrag);
-  box.removeEventListener('pointerup', stopElementDrag);
+    box.releasePointerCapture(e.pointerId);
+    box.removeEventListener('pointermove', onDrag);
+    box.removeEventListener('pointerup', stopElementDrag);
 }
