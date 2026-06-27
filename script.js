c8b40be6d3e74f498997f9c6f1c1f1f8ca1376bb
Kv1st3n <233747346+Kv1st3n@users.noreply.github.com>
2026-06-27 17:08:26 +0200

    update

--- script.js
+++ script.js
@@ -24,30 +24,30 @@ draggableElements.forEach((element) => {
     let offsetX = 0;
     let offsetY = 0;
 
-  element.addEventListener('pointerdown', (e) => {
-    draggableElements.forEach(el => el.style.zIndex = "5");
-    element.style.zIndex = "10";
+    element.addEventListener('pointerdown', (e) => {
+        draggableElements.forEach(el => el.style.zIndex = "5");
+        element.style.zIndex = "10";
 
-    element.setPointerCapture(e.pointerId);
+        element.setPointerCapture(e.pointerId);
+        
+        offsetX = e.clientX - element.offsetLeft;
+        offsetY = e.clientY - element.offsetTop;
     
-    offsetX = e.clientX - element.offsetLeft;
-    offsetY = e.clientY - element.offsetTop;
-    
-    // updates the position
-    const onDrag = (moveEvent) => {
-        element.style.position = 'absolute';
-        element.style.margin = '0'; 
-        element.style.left = `${moveEvent.clientX - offsetX}px`;
-        element.style.top = `${moveEvent.clientY - offsetY}px`;
-    };
+        // updates the position
+        const onDrag = (moveEvent) => {
+            element.style.position = 'absolute';
+            element.style.margin = '0'; 
+            element.style.left = `${moveEvent.clientX - offsetX}px`;
+            element.style.top = `${moveEvent.clientY - offsetY}px`;
+        };
 
     // once the pointer stops dragging, the program will be placed on the new position
-    const stopElementDrag = (upEvent) => {
-        element.releasePointerCapture(upEvent.pointerId);
-        window.removeEventListener('pointermove', onDrag);
-        window.removeEventListener('pointerup', stopElementDrag);
-    };
-
+        const stopElementDrag = (upEvent) => {
+            element.releasePointerCapture(upEvent.pointerId);
+            window.removeEventListener('pointermove', onDrag);
+            window.removeEventListener('pointerup', stopElementDrag);
+        };
+        
         window.addEventListener('pointermove', onDrag);
         window.addEventListener('pointerup', stopElementDrag);
     });
@@ -55,5 +55,4 @@ draggableElements.forEach((element) => {
     element.addEventListener('dblclick', () => {
         launchProgram(element);
     });
-
 });
